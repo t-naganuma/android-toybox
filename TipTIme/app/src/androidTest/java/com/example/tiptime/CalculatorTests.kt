02fd58e07863ce7d9c7f580e7374c586906f8686
Takuya Naganuma <takuya.naganuma@gmail.com>
2022-12-31 18:56:45 +0900

    instrumental test

--- TipTIme/app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ TipTIme/app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -29,4 +29,34 @@ class CalculatorTests {
         onView(withId(R.id.tip_result))
             .check(matches(withText(containsString("$10.00"))))
     }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.option_eighteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.option_fifteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$7.50"))))
+    }
 }
