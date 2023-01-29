2d55d771b0911c5d3aa5ec6032543601cfa28188
t-naganuma <takuya.naganuma@gmail.com>
2023-01-29 18:02:04 +0900

    Room app

---
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,90 @@
+package com.example.inventory
+
+import androidx.lifecycle.*
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    private fun updateItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+    fun sellItem(item: Item) {
+        if (item.quantityInStock > 0) {
+            val newItem = item.copy(quantityInStock = item.quantityInStock - 1)
+            updateItem(newItem)
+        }
+    }
+
+    private fun getUpdatedItemEntity(itemId: Int, itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            id = itemId,
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun updateItem(itemId: Int, itemName: String, itemPrice: String, itemCount: String) {
+        val updatedItem = getUpdatedItemEntity(itemId, itemName, itemPrice, itemCount)
+        updateItem(updatedItem)
+    }
+
+
+
+    fun deleteItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
+    }
+
+    fun isStockAvailable(item: Item): Boolean {
+        return (item.quantityInStock > 0)
+    }
+
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
+        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    fun retrieveItem(id: Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
+    }
+
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
